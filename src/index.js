0aa759ade2d7267ea296ad3826baabdf3fb7570c
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-03-15 12:32:15 +0200

    Prevent default on form submit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { observerAnchorRef, searchInputRef } from './js/DOMrefs';
 import { galleryRef } from './js/DOMrefs';
 import { renderModalImg } from './js/modalImage';
 import { arrowUpScroll } from './js/arrowUpScroll';
-import { arrowUp } from './js/DOMrefs';
+import { arrowUp, seearchFormRef } from './js/DOMrefs';
 import debounce from 'lodash.debounce';
 
 observer.observe(observerAnchorRef);
 searchInputRef.addEventListener('input', debounce(newQueryLoad, 1000));
+seearchFormRef.addEventListener('submit', (event) => {event.preventDefault()});
 galleryRef.addEventListener('click', renderModalImg);
 arrowUp.addEventListener('click', arrowUpScroll);
 
--- src/js/DOMrefs.js
+++ src/js/DOMrefs.js
@@ -3,8 +3,9 @@ const galleryRef = document.querySelector('.gallery');
 const galleryImgRef = document.querySelector('.photo-card');
 const observerAnchorRef = document.querySelector('.observer-anchor');
 const searchInputRef = document.querySelector('.search-input');
+const seearchFormRef = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
 const modalImgTemplate = document.querySelector('.modal-image-template');
 const arrowUp = document.querySelector('.arrow-up');
 
-export { galleryRef, galleryImgRef, observerAnchorRef, searchInputRef, galleryList, modalImgTemplate, arrowUp };
+export { galleryRef, galleryImgRef, observerAnchorRef, searchInputRef, galleryList, modalImgTemplate, arrowUp, seearchFormRef };
