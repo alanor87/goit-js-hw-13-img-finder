2372768ec3e9e700dd6b4680a14cd55e58513191
Yevhen Martianov <jonny.freeman321@gmail.com>
2021-03-15 13:42:38 +0200

    some changes

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -5,22 +5,25 @@ import '../../node_modules/@pnotify/core/dist/PNotify.css';
 import '../../node_modules/@pnotify/mobile/dist/PNotifyMobile.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import { info } from '@pnotify/core';
-export { fetchImages, newQueryLoad, nextPageLoad };
+export { fetchImages, newQueryLoad, nextPageLoad};
 
 const noticeOptions = {
     shadow: true,
     hide: true,
     delay: 1000,
-}
-const BASE_URL = 'https://pixabay.com/api/';
-const API_TOKEN = '20496318-c7af985c6ce4a327e41f45e16';
-let currentPage = 1;
-let query = 'cats';
+};
 
-function fetchImages() {
+const queryOptions = {
+    BASE_URL: 'https://pixabay.com/api/',
+    API_TOKEN: '20496318-c7af985c6ce4a327e41f45e16',
+    currentPage: 1,
+    query: 'cats',
+}
+function fetchImages({ BASE_URL, API_TOKEN, currentPage, query}) {
     return fetch(`${BASE_URL}?key=${API_TOKEN}&q=${query}&page=${currentPage}&per_page=12`)
         .then(responce => responce.json())
-        .then(renderGallery);
+        .then(renderGallery)
+        .finally(renderNotify(queryOptions, noticeOptions));
 };
 
 function renderGallery(imgArray) {
@@ -31,21 +34,20 @@ function renderGallery(imgArray) {
 
 function nextPageLoad(entries) {
     if (entries[0].isIntersecting) {
-        fetchImages();
-        currentPage += 1;
+        fetchImages(queryOptions);
+        queryOptions.currentPage += 1;
     }
 };
 
 function newQueryLoad() {
-    query = searchInputRef.value;
-    currentPage = 1;
+    queryOptions.query = searchInputRef.value;
+    queryOptions.currentPage = 1;
     galleryRef.innerHTML = '';
-    renderNotify();
-    console.log(query);
+    console.log(queryOptions.query);
 };
 
-function renderNotify() {
-    info({ text: `${query} results`, ...noticeOptions})
+function renderNotify({ query, currentPage }, noticeOptions) {
+    info({ text: `${query} results page ${currentPage}`, ...noticeOptions})
 }
 
 
